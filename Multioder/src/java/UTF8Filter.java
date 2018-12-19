cc180b78cc58475f8382787ed6622f32795452a1
Sedah <phensiritn1@gmail.com>
2018-12-19 08:29:10 +0700

    Update UTF-8 Filter

--- Multioder/src/java/UTF8Filter.java
+++ Multioder/src/java/UTF8Filter.java
@@ -22,19 +22,20 @@ import javax.servlet.annotation.WebFilter;
  */
 @WebFilter(filterName = "UTF8Filter", urlPatterns = {"/*"})
 public class UTF8Filter implements Filter {
-    
+
     private static final boolean debug = true;
 
     // The filter configuration object we are associated with.  If
     // this value is null, this filter instance is not currently
     // configured. 
     private FilterConfig filterConfig = null;
-    
+
     public UTF8Filter() {
-    }    
-    
+    }
+
     private void doBeforeProcessing(ServletRequest request, ServletResponse response)
             throws IOException, ServletException {
+        request.setCharacterEncoding("UTF-8");
         if (debug) {
             log("UTF8Filter:DoBeforeProcessing");
         }
@@ -59,8 +60,8 @@ public class UTF8Filter implements Filter {
 	    log(buf.toString());
 	}
          */
-    }    
-    
+    }
+
     private void doAfterProcessing(ServletRequest request, ServletResponse response)
             throws IOException, ServletException {
         if (debug) {
@@ -98,15 +99,15 @@ public class UTF8Filter implements Filter {
     public void doFilter(ServletRequest request, ServletResponse response,
             FilterChain chain)
             throws IOException, ServletException {
-        
+
         if (debug) {
             log("UTF8Filter:doFilter()");
         }
-        
+
         doBeforeProcessing(request, response);
-        
+
         response.setCharacterEncoding("utf-8");
-        
+
         Throwable problem = null;
         try {
             chain.doFilter(request, response);
@@ -117,7 +118,7 @@ public class UTF8Filter implements Filter {
             problem = t;
             t.printStackTrace();
         }
-        
+
         doAfterProcessing(request, response);
 
         // If there was a problem, we want to rethrow it if it is
@@ -152,16 +153,16 @@ public class UTF8Filter implements Filter {
     /**
      * Destroy method for this filter
      */
-    public void destroy() {        
+    public void destroy() {
     }
 
     /**
      * Init method for this filter
      */
-    public void init(FilterConfig filterConfig) {        
+    public void init(FilterConfig filterConfig) {
         this.filterConfig = filterConfig;
         if (filterConfig != null) {
-            if (debug) {                
+            if (debug) {
                 log("UTF8Filter:Initializing filter");
             }
         }
@@ -180,20 +181,20 @@ public class UTF8Filter implements Filter {
         sb.append(")");
         return (sb.toString());
     }
-    
+
     private void sendProcessingError(Throwable t, ServletResponse response) {
-        String stackTrace = getStackTrace(t);        
-        
+        String stackTrace = getStackTrace(t);
+
         if (stackTrace != null && !stackTrace.equals("")) {
             try {
                 response.setContentType("text/html");
                 PrintStream ps = new PrintStream(response.getOutputStream());
-                PrintWriter pw = new PrintWriter(ps);                
+                PrintWriter pw = new PrintWriter(ps);
                 pw.print("<html>\n<head>\n<title>Error</title>\n</head>\n<body>\n"); //NOI18N
 
                 // PENDING! Localize this for next official release
-                pw.print("<h1>The resource did not process correctly</h1>\n<pre>\n");                
-                pw.print(stackTrace);                
+                pw.print("<h1>The resource did not process correctly</h1>\n<pre>\n");
+                pw.print(stackTrace);
                 pw.print("</pre></body>\n</html>"); //NOI18N
                 pw.close();
                 ps.close();
@@ -210,7 +211,7 @@ public class UTF8Filter implements Filter {
             }
         }
     }
-    
+
     public static String getStackTrace(Throwable t) {
         String stackTrace = null;
         try {
@@ -224,9 +225,9 @@ public class UTF8Filter implements Filter {
         }
         return stackTrace;
     }
-    
+
     public void log(String msg) {
-        filterConfig.getServletContext().log(msg);        
+        filterConfig.getServletContext().log(msg);
     }
-    
+
 }
